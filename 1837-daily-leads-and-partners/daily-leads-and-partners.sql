9966f9d80c8a0a77e77e7df0befd8739f0e71235
Argha Dutta <123383407+ArghaDutta15@users.noreply.github.com>
2025-09-21 15:35:16 +0530

    Time: 1239 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1837-daily-leads-and-partners/daily-leads-and-partners.sql
+++ 1837-daily-leads-and-partners/daily-leads-and-partners.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
 SELECT date_id, make_name,
 count(DISTINCT lead_id) AS unique_leads,
 count(DISTINCT partner_id) AS unique_partners
